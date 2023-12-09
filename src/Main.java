444d9c3508132c02e6257110e1beda681c0c842f
hoanganh3211 <maihoanganh_t67@hus.edu.vn>
2023-12-09 21:59:33 +0700

    sua lai ham Main, thay ten game

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,8 @@ public class Main {
 
       // Bật chế độ fullscreen
       view.setExtendedState(JFrame.MAXIMIZED_BOTH);
-       // Loại bỏ các đối tượng trang trí (title bar, border, etc.)
 
-      // Thực hiện một số công việc sau khi tạo đối tượng View (nếu cần)
-      // Ví dụ: tạo đối tượng Countdown và bắt đầu đếm ngược
-      Countdown countdown = new Countdown();
-      countdown.countdown(10); // Bắt đầu đếm ngược từ 10 giây
 
-      // Các dòng mã khác tại đây nếu có thêm tính năng hoặc tác vụ cần thực hiện
     });
   }
 }
