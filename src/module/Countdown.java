38dc453d7bf7da7ac2bae6c047d9b5484e7f28e8
Anbt0111 <131143530+Anbt0111@users.noreply.github.com>
2023-12-15 01:12:55 +0700

    Update Countdown.java

--- src/module/Countdown.java
+++ src/module/Countdown.java
@@ -28,14 +28,15 @@ public class Countdown implements Runnable {
   }
 
   @Override
-    public void run() {
-        try {
-            while (time > 0) {
-                Thread.sleep(1000);
-                time--;
-            }
-        } catch (InterruptedException e) {
-            return;
-        }
+public void run() {
+    while (time > 0) {
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        return; // thread is interrupted, stop counting, stop thread
+      }
+      time--;
     }
+    time--;
+  }
 }
